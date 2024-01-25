fd917e2f8b26b4d281b54dd81f8b330258a160b3
Jose Saarimaa <jose.saarimaa@stat.fi>
2024-01-25 08:55:47 +0200

    Removed the failing test that was added to test the CI pipeline

--- Px.Utils.UnitTests/TestTest.cs
+++ Px.Utils.UnitTests/TestTest.cs
@@ -8,14 +8,5 @@ namespace Px.Utils.UnitTests
             InitTest initTest = new();
             Assert.Equal("Hello World", initTest.Test());
         }
-
-        /// <summary>
-        /// This test should fail. It is used to test the CI/CD pipeline and will be removed when the testing is done.
-        /// </summary>
-        [Fact]
-        public void FailTest1()
-        {
-            Assert.True(false);
-        }
     }
 }
