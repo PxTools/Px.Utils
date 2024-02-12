b100d62734d3aabeeff1ad89e0abfe87ff38136c
Jose Saarimaa <jose.saarimaa@stat.fi>
2024-02-12 12:48:29 +0200

    Fixed bad namespaces

--- Px.Utils.UnitTests/PxFileTests/PxFileMetadataReaderTests/ReadMetadataAsyncTests.cs
+++ Px.Utils.UnitTests/PxFileTests/PxFileMetadataReaderTests/ReadMetadataAsyncTests.cs
@@ -1,7 +1,7 @@
 ﻿using PxUtils.UnitTests.PxFileTests.Fixtures;
 using PxUtils.PxFile.Meta;
 using System.Text;
-using Px.Utils.Exceptions;
+using PxUtils.Exceptions;
 
 namespace PxFileTests.PxFileMetadataReaderTests
 {
--- Px.Utils/PxFile/PxFileMetadataReader.cs
+++ Px.Utils/PxFile/PxFileMetadataReader.cs
@@ -1,5 +1,4 @@
-﻿using PxUtils.PxFile.Exceptions;
-using System;
+﻿using PxUtils.Exceptions;
 using System.Runtime.CompilerServices;
 using System.Text;
 
