9c0bd2e794628b1618a1215f8bf66c5a3f9a3fa8
Jose Saarimaa <jose.saarimaa@stat.fi>
2024-03-26 15:52:24 +0200

    Added more unit tests for the data reading.

--- Px.Utils.UnitTests/PxFileTests/DataTests/PxFileStreamDataReaderTests/DataReaderTests.cs
+++ Px.Utils.UnitTests/PxFileTests/DataTests/PxFileStreamDataReaderTests/DataReaderTests.cs
@@ -9,7 +9,7 @@ namespace PxFileTests.DataTests.PxFileStreamDataReaderTests
     public class DataReaderTests
     {
         [TestMethod]
-        public void ReadDoubleDataValues_ValidIntegers_ReturnsCorrectDoubleDataValues()
+        public void ReadAddDoubleDataValues_ValidIntegers_ReturnsCorrectDoubleDataValues()
         {
             // Arrange
             byte[] data = Encoding.UTF8.GetBytes(DataReaderFixtures.MINIMAL_UTF8_20DATAVALUES);
@@ -27,6 +27,50 @@ namespace PxFileTests.DataTests.PxFileStreamDataReaderTests
             Assert.AreEqual(9.0, targetBuffer[9].UnsafeValue);
             Assert.AreEqual(10.0, targetBuffer[10].UnsafeValue);
             Assert.AreEqual(19.0, targetBuffer[19].UnsafeValue);
+        }
+
+        [TestMethod]
+        public void ReadEveryOtherDoubleDataValue_ValidIntegers_ReturnsCorrectDoubleDataValues()
+        {
+            // Arrange
+            byte[] data = Encoding.UTF8.GetBytes(DataReaderFixtures.MINIMAL_UTF8_20DATAVALUES);
+            using Stream stream = new MemoryStream(data);
+            using PxFileStreamDataReader reader = new(stream);
+            DoubleDataValue[] targetBuffer = new DoubleDataValue[20];
+
+            // Act
+            int[] rowRange = DataTestHelpers.BuildRanges(0, 2, 2);
+            int[] colRange = DataTestHelpers.BuildRanges(0, 10, 2);
+            reader.ReadDoubleDataValues(targetBuffer, 0, rowRange, colRange);
+
+            // Assert
+            Assert.AreEqual(0.0, targetBuffer[0].UnsafeValue);
+            Assert.AreEqual(2.0, targetBuffer[1].UnsafeValue);
+            Assert.AreEqual(4.0, targetBuffer[2].UnsafeValue);
+            Assert.AreEqual(6.0, targetBuffer[3].UnsafeValue);
+            Assert.AreEqual(8.0, targetBuffer[4].UnsafeValue);
+        }
+
+        [TestMethod]
+        public void ReadEveryOtherDoubleDataValueFrom2ndRow_ValidIntegers_ReturnsCorrectDoubleDataValues()
+        {
+            // Arrange
+            byte[] data = Encoding.UTF8.GetBytes(DataReaderFixtures.MINIMAL_UTF8_20DATAVALUES);
+            using Stream stream = new MemoryStream(data);
+            using PxFileStreamDataReader reader = new(stream);
+            DoubleDataValue[] targetBuffer = new DoubleDataValue[20];
+
+            // Act
+            int[] rowRange = DataTestHelpers.BuildRanges(1, 2, 1);
+            int[] colRange = DataTestHelpers.BuildRanges(0, 10, 2);
+            reader.ReadDoubleDataValues(targetBuffer, 0, rowRange, colRange);
+
+            // Assert
+            Assert.AreEqual(10.0, targetBuffer[0].UnsafeValue);
+            Assert.AreEqual(12.0, targetBuffer[1].UnsafeValue);
+            Assert.AreEqual(14.0, targetBuffer[2].UnsafeValue);
+            Assert.AreEqual(16.0, targetBuffer[3].UnsafeValue);
+            Assert.AreEqual(18.0, targetBuffer[4].UnsafeValue);
         }
     }
 }
