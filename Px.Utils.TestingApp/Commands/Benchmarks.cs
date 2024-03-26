fa65c1ed1a24a40f74a81c1934263aa2916f1c3e
Jose Saarimaa <jose.saarimaa@stat.fi>
2024-03-26 13:10:40 +0200

    Added an initial project for a testing application.

---
+++ Px.Utils.TestingApp/Commands/Benchmarks.cs
@@ -0,0 +1,42 @@
+﻿namespace Px.Utils.TestingApp.Commands
+{
+    internal abstract class Benchmarks
+    {
+        internal class BenchmarkResult
+        {
+            internal readonly string Name;
+            internal bool Error { get; }
+            internal IReadOnlyList<double> IterationTimesMs { get; }
+            internal double AverageTimeMs => IterationTimesMs.Average();
+            internal double MinTimeMs => IterationTimesMs.Min();
+            internal double MaxTimeMs => IterationTimesMs.Max();
+            internal double MedianTimeMs
+            {
+                get
+                {
+                    List<double> sortedTimes = [.. IterationTimesMs.OrderBy(x => x)];
+                    return sortedTimes[sortedTimes.Count / 2];
+                }
+            }
+
+            internal BenchmarkResult(string name, IReadOnlyList<double> iterationTimesMs)
+            {
+                Name = name;
+                IterationTimesMs = iterationTimesMs;
+            }
+        }
+
+        internal int Iterations { get; set; } = 10;
+
+        internal List<BenchmarkResult> Results { get; } = [];
+
+        internal abstract void Run();
+
+        internal void PrintResults()
+        {
+            Console.WriteLine($"Benchmark results for {Iterations} iterations:");
+            Console.WriteLine("| Name\t\t| Min ms.\t| Max ms. \t| Avarage ms. \t| Median ms. \t|");
+            Console.WriteLine("|---------------|---------------|---------------|---------------|---------------|");
+        }
+    }
+}
---
+++ Px.Utils.TestingApp/TestingApp.cs
@@ -0,0 +1,24 @@
+﻿using Px.Utils.TestingApp.Commands;
+
+namespace Px.Utils.TestingApp
+{
+    internal class TestingApp
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length == 0) StartInteractiveMode();
+            else StartBatchMode(args);
+        }
+
+        static void StartInteractiveMode()
+        {
+            DataReadBenchmarks benchmarks = new();
+            benchmarks.PrintResults();
+        }
+
+        static void StartBatchMode(string[] _)
+        {
+            Console.WriteLine("Batch mode is not yet supported.");
+        }
+    }
+}
