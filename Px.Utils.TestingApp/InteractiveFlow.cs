0f6d2f9d2d07be0772b13b34e184db902c49eed9
Jose Saarimaa <jose.saarimaa@stat.fi>
2024-03-28 15:02:13 +0200

    Improved the benchmarking application

---
+++ Px.Utils.TestingApp/InteractiveFlow.cs
@@ -0,0 +1,27 @@
+﻿using Px.Utils.TestingApp;
+using Px.Utils.TestingApp.Commands;
+
+namespace PxUtils.TestingApp
+{
+    internal class InteractiveFlow
+    {
+        private readonly Dictionary<string, Command> _commands = [];
+
+        internal InteractiveFlow()
+        {
+            _commands.Add("bench", new BenchmarkRunner());
+        }
+
+        public void Start()
+        {
+            string q = "Enter the command and optional parameters:";
+            string followUp = "Valid command is required, please input the command:";
+            List<string> inputs = TestAppConsole.AskQuestion(q, true, followUp);
+            while (!_commands.ContainsKey(inputs[0]))
+            {
+                inputs = TestAppConsole.AskQuestion(followUp, true);
+            }
+            _commands[inputs[0]].Run(false, inputs.Skip(1).ToList());
+        }
+    }
+}
--- Px.Utils.UnitTests/PxFileTests/Fixtures/DataReaderFixtures.cs
+++ Px.Utils.UnitTests/PxFileTests/Fixtures/DataReaderFixtures.cs
@@ -14,5 +14,18 @@
             "DATA=\n" +
             "0 1 2 3 4 5 6 7 8 9 \n" +
             "10 11 12 13 14 15 16 17 18 19;";
+
+        internal static string MINIMAL_UTF8_20DECIMALVALUES =>
+            "CHARSET=\"Unicode\";\n" +
+            "AXIS-VERSION=\"2013\";\n" +
+            "CODEPAGE=\"utf-8\";\n" +
+            "LANGUAGES=\"aa\",\"åå\",\"öö\";\n" +
+            "NEXT-UPDATE=\"20240131 08:00\";\n" +
+            "SUBJECT-AREA=\"test\";\n" +
+            "SUBJECT-AREA[åå]=\"test\";\n" +
+            "COPYRIGHT=YES;\n" +
+            "DATA=\n" +
+            "0.0 0.01 0.02 0.03 0.04 0.05 0.06 0.07 0.08 0.09 \n" +
+            "0.1 0.11 0.12 0.13 0.14 0.15 0.16 0.17 0.18 0.19;";
     }
 }
