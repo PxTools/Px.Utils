0c0fc9c93053a6c7619e720966e0d83f42b3df9f
Jose Saarimaa <jose.saarimaa@stat.fi>
2024-05-02 14:35:46 +0300

    Implementations for the IDataValue interface

--- Px.Utils/Models/Data/IDataValue.cs
+++ Px.Utils/Models/Data/IDataValue.cs
@@ -2,14 +2,16 @@
 
 namespace Px.Utils.Models.Data
 {
+    /// <summary>
+    /// Interface for wrapping the basic arithmetic operations for a data value.
+    /// </summary>
+    /// <typeparam name="TSelf"></typeparam>
     public interface IDataValue<TSelf> :
           IAdditionOperators<TSelf, TSelf, TSelf>,
           IAdditiveIdentity<TSelf, TSelf>,
-          IDecrementOperators<TSelf>,
           IDivisionOperators<TSelf, TSelf, TSelf>,
           IEquatable<TSelf>,
           IEqualityOperators<TSelf, TSelf, bool>,
-          IIncrementOperators<TSelf>,
           IMultiplicativeIdentity<TSelf, TSelf>,
           IMultiplyOperators<TSelf, TSelf, TSelf>,
           ISubtractionOperators<TSelf, TSelf, TSelf>,
