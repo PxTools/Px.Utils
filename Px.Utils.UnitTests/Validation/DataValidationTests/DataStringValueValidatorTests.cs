77614a1e28bd78f2c57fe4c6f5b64690d692b370
Sakari Malkki <sakari.malkki@stat.fi>
2024-05-08 10:15:18 +0300

    More string validation tests

--- Px.Utils.UnitTests/Validation/DataValidationTests/DataStringValueValidatorTests.cs
+++ Px.Utils.UnitTests/Validation/DataValidationTests/DataStringValueValidatorTests.cs
@@ -25,21 +25,30 @@ namespace Px.Utils.UnitTests.Validation.DataValidationTests
 
             Assert.IsNull(nullableFeedback);
         }
-    
+
         [TestMethod]
-        public void NotAllowedStringValue()
+        [DataRow("\"...")]
+        [DataRow("...\"")]
+        [DataRow("\"\"")]
+        [DataRow("...")]
+        [DataRow("foo")]
+        [DataRow("\"foo\"")]
+        [DataRow("\".......\"")]
+        [DataRow("\"--\"")]
+        [DataRow("-")]
+        public void NotAllowedStringValue(string notAllowedValue)
         {
             DataStringValidator validator = new();
 
             Encoding encoding = Encoding.UTF8;
-            List<byte> value = [.. encoding.GetBytes("X")];
+            List<byte> value = [.. encoding.GetBytes(notAllowedValue)];
 
             ValidationFeedback? nullableFeedback = validator.Validate(value, EntryType.DataItem, encoding, 0, 0);
 
             Assert.IsNotNull(nullableFeedback);
             ValidationFeedback feedback = (ValidationFeedback)nullableFeedback;
             Assert.AreEqual(ValidationFeedbackRule.DataValidationFeedbackInvalidString, feedback.Rule);
-            Assert.AreEqual("X", feedback.AdditionalInfo);
+            Assert.AreEqual(notAllowedValue, feedback.AdditionalInfo);
             Assert.AreEqual(ValidationFeedbackLevel.Error, feedback.Level);
         }
     }
