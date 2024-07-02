9f57b02e5be66016fc2d32122c860ff564c4a87e
Jose Saarimaa <jose.saarimaa@stat.fi>
2024-07-02 08:53:18 +0300

    Added documentation.

--- Px.Utils/Database/PxTableReference.cs
+++ Px.Utils/Database/PxTableReference.cs
@@ -1,19 +1,36 @@
 ﻿
 namespace Px.Utils.Database
 {
+    /// <summary>
+    /// Reference to a px-file that contains a unique identifier of the file and the last write time.
+    /// </summary>
     public readonly record struct PxTableReference
     {
+        /// <summary>
+        /// Identifies the file
+        /// </summary>
         public readonly string Identifier { get; }
 
+        /// <summary>
+        /// When whas the file last written to.
+        /// </summary>
         public readonly DateTime TableLastUpdated { get; }
 
+        /// <summary>
+        /// Creation time of the reference.
+        /// </summary>
         public readonly DateTime ReferenceCreated { get; }
 
-        public PxTableReference(string identifier, DateTime tableLastUpdated, DateTime referenceCreated)
+        /// <summary>
+        /// Default constructor.
+        /// </summary>
+        /// <param name="identifier">Unique identifier of the file.</param>
+        /// <param name="tableLastUpdated">When was the table last written to.</param>
+        public PxTableReference(string identifier, DateTime tableLastUpdated)
         {
             Identifier = identifier;
             TableLastUpdated = tableLastUpdated;
-            ReferenceCreated = referenceCreated;
+            ReferenceCreated = DateTime.Now;
         }
     }
 }
