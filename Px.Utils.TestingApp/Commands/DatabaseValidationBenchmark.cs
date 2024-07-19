e79ad474a6d8788f23a5f4062d5809bbdf399742
Sakari Malkki <sakari.malkki@stat.fi>
2024-07-19 11:01:19 +0300

    Benchmarks for database validation

---
+++ Px.Utils.TestingApp/Commands/DatabaseValidationBenchmark.cs
@@ -0,0 +1,36 @@
+﻿using Px.Utils.Validation.DatabaseValidation;
+
+namespace Px.Utils.TestingApp.Commands
+{
+    internal class DatabaseValidationBenchmark : FileBenchmark
+    {
+        internal override string Help => "Validates a px file database.";
+
+        internal override string Description => "Validates a px file database.";
+
+        private DatabaseValidator validator;
+
+        internal DatabaseValidationBenchmark()
+        { 
+            BenchmarkFunctions = [ValidationBenchmark];
+            BenchmarkFunctionsAsync = [ValidationBenchmarkAsync];
+        }
+
+        protected override void OneTimeBenchmarkSetup()
+        {
+            base.OneTimeBenchmarkSetup();
+
+            validator = new(TestFilePath);
+        }
+
+        protected void ValidationBenchmark()
+        {
+            validator.Validate();
+        }
+
+        protected async Task ValidationBenchmarkAsync()
+        {
+            await validator.ValidateAsync();
+        }
+    }
+}
