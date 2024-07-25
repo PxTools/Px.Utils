f2e9bbd0919b3176d945b74f7255f123279ebe43
Sakari Malkki <sakari.malkki@stat.fi>
2024-07-25 12:45:57 +0300

    Matrix metadata extension methods for finding content and time dimensions

---
+++ Px.Utils/Models/Metadata/MatrixMetadataExtensions.cs
@@ -0,0 +1,19 @@
+﻿using Px.Utils.Models.Metadata.Dimensions;
+
+namespace Px.Utils.Models.Metadata
+{
+    public static class MatrixMetadataExtensions
+    {
+        public static ContentDimension GetContentDimension(this MatrixMetadata metadata)
+        {
+            ContentDimension? ct = metadata.Dimensions.OfType<ContentDimension>().FirstOrDefault();
+            return ct ?? throw new InvalidOperationException("Content dimension not found in metadata");
+        }
+
+        public static TimeDimension GetTimeDimension(this MatrixMetadata metadata)
+        {             
+            TimeDimension? time = metadata.Dimensions.OfType<TimeDimension>().FirstOrDefault();
+            return time ?? throw new InvalidOperationException("Time dimension not found in metadata");
+        }
+    }
+}
