5d63b2d6506b0f76974c6dc56649fa8ef7ea37af
Jose Saarimaa <jose.saarimaa@stat.fi>
2025-07-30 09:45:38 +0300

    Added submap/supermap extensions for the matrix map. Fixed an issue in matrix map build that could potentially cause unexpected memory consumption.

---
+++ Px.Utils/Models/Metadata/ExtensionMethods/DimensionMapExtensions.cs
@@ -0,0 +1,42 @@
+﻿namespace Px.Utils.Models.Metadata.ExtensionMethods
+{
+    public static class DimensionMapExtensions
+    {
+        public static bool IsSubmapOf(this IDimensionMap thisMap, IDimensionMap other)
+        {
+            if (thisMap.Code != other.Code) return false;
+            int sourceIndex = 0;
+            for (int j = 0; j < thisMap.ValueCodes.Count; j++)
+            {
+                bool found = false;
+                for (int k = sourceIndex; k < other.ValueCodes.Count; k++)
+                {
+                    if (other.ValueCodes[k] == thisMap.ValueCodes[j])
+                    {
+                        sourceIndex = k + 1;
+                        found = true;
+                        break;
+                    }
+                }
+                if (!found) return false;
+            }
+            return true;
+        }
+
+        public static bool IsSupermapOf(this IDimensionMap thisMap, IDimensionMap other)
+        {
+            return other.IsSubmapOf(thisMap);
+        }
+
+        public static bool IsIdenticalMapTo(this IDimensionMap thisMap, IDimensionMap other)
+        {
+            if (thisMap.Code != other.Code) return false;
+            if (thisMap.ValueCodes.Count != other.ValueCodes.Count) return false;
+            for (int i = 0; i < thisMap.ValueCodes.Count; i++)
+            {
+                if (thisMap.ValueCodes[i] != other.ValueCodes[i]) return false;
+            }
+            return true;
+        }
+    }
+}
