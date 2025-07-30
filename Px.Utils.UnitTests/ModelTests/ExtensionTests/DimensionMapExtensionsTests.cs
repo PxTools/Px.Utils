3be705a6ce918e0bb7fcb235c14774f4e635dea7
Sakari Malkki <sakari.malkki@stat.fi>
2025-07-30 13:05:00 +0300

    Summary documentation, dimension map extension tests, matrix map extension tests and readme update

---
+++ Px.Utils.UnitTests/ModelTests/ExtensionTests/DimensionMapExtensionsTests.cs
@@ -0,0 +1,181 @@
+﻿using Px.Utils.Models.Metadata;
+using Px.Utils.Models.Metadata.ExtensionMethods;
+
+namespace Px.Utils.UnitTests.ModelTests.ExtensionTests
+{
+    [TestClass]
+    public class DimensionMapExtensionsTests
+    {
+        #region IsSubmapOf
+
+        [TestMethod]
+        public void IsSubmapOfIdenticalMapsReturnsTrue()
+        {
+            IDimensionMap map1 = new DimensionMap("dim1", ["val1", "val2"]);
+            IDimensionMap map2 = new DimensionMap("dim1", ["val1", "val2"]);
+            Assert.IsTrue(map1.IsSubmapOf(map2));
+        }
+
+        [TestMethod]
+        public void IsSubmapOfDifferentCodesReturnsFalse()
+        {
+            IDimensionMap map1 = new DimensionMap("dim1", ["val1", "val2"]);
+            IDimensionMap map2 = new DimensionMap("dim2", ["val1", "val2"]);
+            Assert.IsFalse(map1.IsSubmapOf(map2));
+        }
+
+        [TestMethod]
+        public void IsSubmapOfSameCodeDifferentValuesReturnsFalse()
+        {
+            IDimensionMap map1 = new DimensionMap("dim1", ["val1", "val2"]);
+            IDimensionMap map2 = new DimensionMap("dim1", ["val1", "val3"]);
+            Assert.IsFalse(map1.IsSubmapOf(map2));
+        }
+
+        [TestMethod]
+        public void IsSubmapOfContainsAllValuesInOrderReturnsTrue()
+        {
+            IDimensionMap map1 = new DimensionMap("dim1", ["val1", "val2"]);
+            IDimensionMap map2 = new DimensionMap("dim1", ["val0", "val1", "val2", "val3"]);
+            Assert.IsTrue(map1.IsSubmapOf(map2));
+        }
+
+        [TestMethod]
+        public void IsSubmapOfContainsValuesOutOfOrderReturnsFalse()
+        {
+            IDimensionMap map1 = new DimensionMap("dim1", ["val1", "val2"]);
+            IDimensionMap map2 = new DimensionMap("dim1", ["val2", "val1"]);
+            Assert.IsFalse(map1.IsSubmapOf(map2));
+        }
+
+        [TestMethod]
+        public void IsSubmapOfEmptyMapReturnsTrue()
+        {
+            IDimensionMap map1 = new DimensionMap("dim1", []);
+            IDimensionMap map2 = new DimensionMap("dim1", ["val1", "val2"]);
+            Assert.IsTrue(map1.IsSubmapOf(map2));
+        }
+
+        [TestMethod]
+        public void IsSubmapOfEmptyMapWithSameCodeReturnsTrue()
+        {
+            IDimensionMap map1 = new DimensionMap("dim1", []);
+            IDimensionMap map2 = new DimensionMap("dim1", []);
+            Assert.IsTrue(map1.IsSubmapOf(map2));
+        }
+
+        #endregion
+
+        #region IsSupermapOf
+
+        [TestMethod]
+        public void IsSupermapOfIdenticalMapsReturnsTrue()
+        {
+            IDimensionMap map1 = new DimensionMap("dim1", ["val1", "val2"]);
+            IDimensionMap map2 = new DimensionMap("dim1", ["val1", "val2"]);
+            Assert.IsTrue(map1.IsSupermapOf(map2));
+        }
+
+        [TestMethod]
+        public void IsSupermapOfDifferentCodesReturnsFalse()
+        {
+            IDimensionMap map1 = new DimensionMap("dim1", ["val1", "val2"]);
+            IDimensionMap map2 = new DimensionMap("dim2", ["val1", "val2"]);
+            Assert.IsFalse(map1.IsSupermapOf(map2));
+        }
+
+        [TestMethod]
+        public void IsSupermapOfSameCodeDifferentValuesReturnsFalse()
+        {
+            IDimensionMap map1 = new DimensionMap("dim1", ["val1", "val2"]);
+            IDimensionMap map2 = new DimensionMap("dim1", ["val1", "val3"]);
+            Assert.IsFalse(map1.IsSupermapOf(map2));
+        }
+
+        [TestMethod]
+        public void IsSupermapOfContainsAllValuesInOrderReturnsTrue()
+        {
+            IDimensionMap map1 = new DimensionMap("dim1", ["val0", "val1", "val2", "val3"]);
+            IDimensionMap map2 = new DimensionMap("dim1", ["val1", "val2"]);
+            Assert.IsTrue(map1.IsSupermapOf(map2));
+        }
+
+        [TestMethod]
+        public void IsSupermapOfContainsValuesOutOfOrderReturnsFalse()
+        {
+            IDimensionMap map1 = new DimensionMap("dim1", ["val2", "val1"]);
+            IDimensionMap map2 = new DimensionMap("dim1", ["val1", "val2"]);
+            Assert.IsFalse(map1.IsSupermapOf(map2));
+        }
+
+        [TestMethod]
+        public void IsSupermapOfEmptyMapReturnsTrue()
+        {
+            IDimensionMap map1 = new DimensionMap("dim1", ["val1", "val2"]);
+            IDimensionMap map2 = new DimensionMap("dim1", []);
+            Assert.IsTrue(map1.IsSupermapOf(map2));
+        }
+
+        [TestMethod]
+        public void IsSupermapOfEmptyMapWithSameCodeReturnsTrue()
+        {
+            IDimensionMap map1 = new DimensionMap("dim1", []);
+            IDimensionMap map2 = new DimensionMap("dim1", []);
+            Assert.IsTrue(map1.IsSupermapOf(map2));
+        }
+
+        #endregion
+
+        #region IsIdenticalMapTo
+
+        [TestMethod]
+        public void IsIdenticalMapToIdenticalMapsReturnsTrue()
+        {
+            IDimensionMap map1 = new DimensionMap("dim1", ["val1", "val2"]);
+            IDimensionMap map2 = new DimensionMap("dim1", ["val1", "val2"]);
+            Assert.IsTrue(map1.IsIdenticalMapTo(map2));
+        }
+
+        [TestMethod]
+        public void IsIdenticalMapToDifferentCodeReturnsFalse()
+        {
+            IDimensionMap map1 = new DimensionMap("dim1", ["val1", "val2"]);
+            IDimensionMap map2 = new DimensionMap("dim2", ["val1", "val2"]);
+            Assert.IsFalse(map1.IsIdenticalMapTo(map2));
+        }
+
+        [TestMethod]
+        public void IsIdenticalMapToValuesInDifferentOrderReturnsFalse()
+        {
+            IDimensionMap map1 = new DimensionMap("dim1", ["val1", "val2"]);
+            IDimensionMap map2 = new DimensionMap("dim1", ["val2", "val1"]);
+            Assert.IsFalse(map1.IsIdenticalMapTo(map2));
+        }
+
+        [TestMethod]
+        public void IsIdenticalMapToDifferentValuesReturnsFalse()
+        {
+            IDimensionMap map1 = new DimensionMap("dim1", ["val1", "val2"]);
+            IDimensionMap map2 = new DimensionMap("dim1", ["val3", "val4"]);
+            Assert.IsFalse(map1.IsIdenticalMapTo(map2));
+        }
+
+        [TestMethod]
+        public void IsIdenticalMapToSubmapReturnsFalse()
+        {
+            IDimensionMap map1 = new DimensionMap("dim1", ["val1", "val2", "val3", "val4"]);
+            IDimensionMap map2 = new DimensionMap("dim1", ["val1", "val2"]);
+            Assert.IsFalse(map1.IsIdenticalMapTo(map2));
+        }
+
+        [TestMethod]
+        public void IsIdenticalMapToSuperReturnsFalse()
+        {
+            IDimensionMap map1 = new DimensionMap("dim1", ["val1", "val2"]);
+            IDimensionMap map2 = new DimensionMap("dim1", ["val1", "val2", "val3", "val4"]);
+            Assert.IsFalse(map1.IsIdenticalMapTo(map2));
+        }
+
+        #endregion
+    }
+}
--- Px.Utils/Models/Metadata/ExtensionMethods/DimensionMapExtensions.cs
+++ Px.Utils/Models/Metadata/ExtensionMethods/DimensionMapExtensions.cs
@@ -1,7 +1,15 @@
 ﻿namespace Px.Utils.Models.Metadata.ExtensionMethods
 {
+    /// <summary>
+    /// Contains extension methods for <see cref="IDimensionMap"/>.
+    /// </summary>
     public static class DimensionMapExtensions
     {
+        /// <summary>
+        /// Checks if the other dimension map has the same code and contains all the values of this map in the same order. 
+        /// The other map can contain additional values that are not found in this map.
+        /// </summary>
+        /// <returns>True if the other map contains all values of this map in the same order. Otherwise, false.</returns>
         public static bool IsSubmapOf(this IDimensionMap thisMap, IDimensionMap other)
         {
             if (thisMap.Code != other.Code) return false;
@@ -23,11 +31,19 @@
             return true;
         }
 
+        /// <summary>
+        /// Checks if this dimension map has the same code and contains all the values of the other map in the same order.
+        /// </summary>
+        /// <returns>True if this map contains all values of the other map in the same order. Otherwise, false.</returns>
         public static bool IsSupermapOf(this IDimensionMap thisMap, IDimensionMap other)
         {
             return other.IsSubmapOf(thisMap);
         }
 
+        /// <summary>
+        /// Checks if this dimension map is identical to the other dimension map.
+        /// </summary>
+        /// <returns>True if the code and all values are the same in the same order. Otherwise, false.</returns>
         public static bool IsIdenticalMapTo(this IDimensionMap thisMap, IDimensionMap other)
         {
             if (thisMap.Code != other.Code) return false;
