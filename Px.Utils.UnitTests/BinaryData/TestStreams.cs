8a7dbb5fdcb24006b121d2853c22b9c1e4108279
Jose Saarimaa <jose.saarimaa@stat.fi>
2026-01-29 10:28:09 +0200

    Refactor BinaryDataReader, add submap index mapping
    - Introduced abstract BinaryDataReader base class and static factory
    - Added Create method for codec-based instantiation
    - Moved submap index logic to IMatrixMap.GetIndicesOfSubmap extension
    - Updated BinaryDataReader to use new index mapping and relative seeking
    - Added unit tests for submap index mapping
    - Refactored test streams and improved test assertions

--- Px.Utils.UnitTests/BinaryData/TestStreams.cs
+++ Px.Utils.UnitTests/BinaryData/TestStreams.cs
@@ -1,24 +1,11 @@
 namespace Px.Utils.UnitTests.BinaryData
 {
-    internal sealed class CountingSeekableStream : Stream
+    internal sealed class CountingSeekableStream(byte[] data) : Stream
     {
-        private readonly MemoryStream _inner;
+        private readonly MemoryStream _inner = new(data, writable: false);
         private int _currentWindowBytes;
 
-        public List<long> SeekOffsets { get; } = new();
-
-        public CountingSeekableStream(byte[] data)
-        {
-            _inner = new MemoryStream(data, writable: false);
-        }
-
-        public void CompleteCurrentWindow()
-        {
-            if (_currentWindowBytes > 0)
-            {
-                _currentWindowBytes = 0;
-            }
-        }
+        public List<long> SeekOffsets { get; } = [];
 
         public override bool CanRead => true;
         public override bool CanSeek => true;
@@ -41,10 +28,7 @@ namespace Px.Utils.UnitTests.BinaryData
             {
                 _currentWindowBytes = 0;
             }
-            if (origin == SeekOrigin.Begin)
-            {
-                SeekOffsets.Add(offset);
-            }
+            SeekOffsets.Add(offset);
             return _inner.Seek(offset, origin);
         }
 
@@ -59,14 +43,9 @@ namespace Px.Utils.UnitTests.BinaryData
         }
     }
 
-    internal sealed class NonSeekableReadOnlyStream : Stream
+    internal sealed class NonSeekableReadOnlyStream(byte[] data) : Stream
     {
-        private readonly MemoryStream _inner;
-
-        public NonSeekableReadOnlyStream(byte[] data)
-        {
-            _inner = new MemoryStream(data, writable: false);
-        }
+        private readonly MemoryStream _inner = new(data, writable: false);
 
         public override bool CanRead => true;
         public override bool CanSeek => false;
---
+++ Px.Utils/BinaryData/BinaryDataReaderStatic.cs
@@ -0,0 +1,51 @@
+using Px.Utils.BinaryData.ValueConverters;
+using Px.Utils.Models.Data.DataValue;
+using Px.Utils.Models.Metadata;
+using System.ComponentModel.DataAnnotations;
+
+namespace Px.Utils.BinaryData
+{
+    public abstract class BinaryDataReader
+    {
+        /// <summary>
+        /// Asynchronous chunk provider that returns a readable <see cref="Stream"/> for the requested window of the blob.
+        /// </summary>
+        /// <param name="offset">Byte offset into the blob.</param>
+        /// <param name="length">Requested number of bytes.</param>
+        /// <param name="ct">Cancellation token.</param>
+        public delegate Task<Stream> AsyncChunkProvider(long offset, long length, CancellationToken ct);
+
+
+        /// <summary>
+        /// Creates a <see cref="BinaryDataReader"/> instance specialized for the specified <paramref name="codecType"/>.
+        /// </summary>
+        /// <param name="codecType">The value codec that defines how binary values are decoded.</param>
+        /// <param name="maxWindowSizeBytes">Optional maximum window size in bytes for chunked reads; must be greater than or equal to 1 if specified.</param>
+        /// <param name="mergeCapBytes">Optional maximum merge size in bytes for adjacent chunks; must be greater than or equal to 1 if specified.</param>
+        /// <param name="headerLengthBytes">Optional header length in bytes to skip before the data region; must be greater than or equal to 0 if specified.</param>
+        /// <returns>A <see cref="BinaryDataReader"/> specialized for the provided codec type.</returns>
+        /// <exception cref="NotSupportedException">Thrown when the specified <paramref name="codecType"/> is not supported.</exception>
+        public static BinaryDataReader Create(
+            BinaryValueCodecType codecType,
+            [Range(1, long.MaxValue)] long? maxWindowSizeBytes = null,
+            [Range(1, long.MaxValue)] long? mergeCapBytes = null,
+            [Range(0, long.MaxValue)] long? headerLengthBytes = null)
+        {
+            return codecType switch
+            {
+                BinaryValueCodecType.UInt16Codec => new BinaryDataReader<UInt16Codec>(maxWindowSizeBytes, mergeCapBytes, headerLengthBytes),
+                BinaryValueCodecType.Int16Codec => new BinaryDataReader<Int16Codec>(maxWindowSizeBytes, mergeCapBytes, headerLengthBytes),
+                BinaryValueCodecType.UInt24Codec => new BinaryDataReader<UInt24Codec>(maxWindowSizeBytes, mergeCapBytes, headerLengthBytes),
+                BinaryValueCodecType.Int24Codec => new BinaryDataReader<Int24Codec>(maxWindowSizeBytes, mergeCapBytes, headerLengthBytes),
+                BinaryValueCodecType.UInt32Codec => new BinaryDataReader<UInt32Codec>(maxWindowSizeBytes, mergeCapBytes, headerLengthBytes),
+                BinaryValueCodecType.Int32Codec => new BinaryDataReader<Int32Codec>(maxWindowSizeBytes, mergeCapBytes, headerLengthBytes),
+                BinaryValueCodecType.FloatCodec => new BinaryDataReader<FloatCodec>(maxWindowSizeBytes, mergeCapBytes, headerLengthBytes),
+                BinaryValueCodecType.DoubleCodec => new BinaryDataReader<DoubleCodec>(maxWindowSizeBytes, mergeCapBytes, headerLengthBytes),
+                _ => throw new NotSupportedException($"Codec type {codecType} is not supported."),
+            };
+        }
+
+        public abstract Task ReadByChunkAsync(AsyncChunkProvider provider, IMatrixMap readMap, IMatrixMap blobMap, IMatrixMap bufferMap, Memory<DoubleDataValue> buffer, CancellationToken ct);
+        public abstract Task ReadFromStreamAsync(Stream source, IMatrixMap readMap, IMatrixMap blobMap, IMatrixMap bufferMap, Memory<DoubleDataValue> buffer, CancellationToken ct);
+    }
+}
