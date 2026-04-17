2a5a7e992f8eb549b18684e32429368a49fb80f9
Sakari Malkki <sakari.malkki@stat.fi>
2026-04-17 10:46:02 +0300

    Test cases for inconsistent linebreaks + simplified data validation tests

--- Px.Utils.UnitTests/Validation/DataValidationTests/DataValidationTest.cs
+++ Px.Utils.UnitTests/Validation/DataValidationTests/DataValidationTest.cs
@@ -1,5 +1,5 @@
+using System.Linq;
 using System.Text;
-using Microsoft.VisualStudio.TestTools.UnitTesting.Logging;
 using Px.Utils.UnitTests.Validation.Fixtures;
 using Px.Utils.Validation;
 using Px.Utils.Validation.DataValidation;
@@ -11,287 +11,71 @@ namespace Px.Utils.UnitTests.Validation.DataValidationTests
     {
         public TestContext TestContext { get; set; }
 
+        private const int DataStartOffset = 6;
+
         [TestMethod]
-        public void TestValidateWithoutErrors()
+        [DataRow(DataStreamContents.SIMPLE_VALID_DATA, 0, 0)]
+        [DataRow(DataStreamContents.SIMPLE_VALID_DATA_WITH_INCONSISTENT_LINEBREAKS, 0, 0)]
+        [DataRow(DataStreamContents.SIMPLE_VALID_DATA_WITHOUT_MISISNG_CODE_DELIMETERS, 0, 0)]
+        [DataRow(DataStreamContents.SIMPLE_INVALID_DATA, 7, 12)]
+        [DataRow(DataStreamContents.NO_DATA, 2, 6, false)]
+        [DataRow(DataStreamContents.DATA_ON_SINGLE_ROW, 2, 6, false)]
+        [DataRow(DataStreamContents.DATA_STARTING_WITH_ENCLOSED_MISSING_VALUE, 0, 0)]
+        [DataRow(DataStreamContents.DATA_STARTING_WITH_UNENCLOSED_MISSING_VALUE, 0, 0)]
+        [DataRow(DataStreamContents.DATA_STARTING_WITH_NIL_VALUE, 0, 0)]
+        [DataRow(DataStreamContents.DATA_STARTING_WITH_UNENCLOSED_NIL_VALUE, 0, 0)]
+        public void ValidateDataReturnsExpectedErrorCount(
+            string dataContents,
+            int expectedUniqueErrorCount,
+            int expectedTotalErrorCount,
+            bool shouldSeekDataStart = true)
         {
-            using Stream stream = new MemoryStream(Encoding.UTF8.GetBytes(DataStreamContents.SIMPLE_VALID_DATA));
-            stream.Seek(6, 0);
+            using Stream stream = new MemoryStream(Encoding.UTF8.GetBytes(dataContents));
+            if (shouldSeekDataStart)
+            {
+                stream.Seek(DataStartOffset, SeekOrigin.Begin);
+            }
+
             DataValidator validator = new(5, 4, 1);
 
             ValidationFeedback validationFeedbacks = validator.Validate(stream, "foo", Encoding.UTF8).FeedbackItems;
+            int actualErrorCount = validationFeedbacks.Values.SelectMany(f => f).Count();
 
-            foreach (KeyValuePair<ValidationFeedbackKey, List<ValidationFeedbackValue>> validationFeedback in validationFeedbacks)
-            {
-                foreach (ValidationFeedbackValue instance in validationFeedback.Value)
-                {
-                    Logger.LogMessage($"Line {instance.Line}, Char {instance.Character}: " 
-                        + $"{validationFeedback.Key.Rule} {instance.AdditionalInfo}");
-                }
-            }
-            Assert.HasCount(0, validationFeedbacks);
+            Assert.HasCount(expectedUniqueErrorCount, validationFeedbacks);
+            Assert.AreEqual(expectedTotalErrorCount, actualErrorCount);
         }
 
         [TestMethod]
-        public void TestValidateWithoutMissingCodeDelimetersReturnsWithoutErrors()
+        [DataRow(DataStreamContents.SIMPLE_VALID_DATA, 0, 0)]
+        [DataRow(DataStreamContents.SIMPLE_VALID_DATA_WITH_INCONSISTENT_LINEBREAKS, 0, 0)]
+        [DataRow(DataStreamContents.SIMPLE_VALID_DATA_WITHOUT_MISISNG_CODE_DELIMETERS, 0, 0)]
+        [DataRow(DataStreamContents.SIMPLE_INVALID_DATA, 7, 12)]
+        [DataRow(DataStreamContents.NO_DATA, 2, 6, false)]
+        [DataRow(DataStreamContents.DATA_ON_SINGLE_ROW, 2, 6, false)]
+        [DataRow(DataStreamContents.DATA_STARTING_WITH_ENCLOSED_MISSING_VALUE, 0, 0)]
+        [DataRow(DataStreamContents.DATA_STARTING_WITH_UNENCLOSED_MISSING_VALUE, 0, 0)]
+        [DataRow(DataStreamContents.DATA_STARTING_WITH_NIL_VALUE, 0, 0)]
+        [DataRow(DataStreamContents.DATA_STARTING_WITH_UNENCLOSED_NIL_VALUE, 0, 0)]
+        public async Task ValidateAsyncDataReturnsExpectedErrorCount(
+            string dataContents,
+            int expectedUniqueErrorCount,
+            int expectedTotalErrorCount,
+            bool shouldSeekToDataStart = true)
         {
-            using Stream stream = new MemoryStream(Encoding.UTF8.GetBytes(DataStreamContents.SIMPLE_VALID_DATA_WITHOUT_MISISNG_CODE_DELIMETERS));
-            stream.Seek(6, 0);
-            DataValidator validator = new(5, 4, 1);
-
-            ValidationFeedback validationFeedbacks = validator.Validate(stream, "foo", Encoding.UTF8).FeedbackItems;
-
-            foreach (KeyValuePair<ValidationFeedbackKey, List<ValidationFeedbackValue>> validationFeedback in validationFeedbacks)
+            using Stream stream = new MemoryStream(Encoding.UTF8.GetBytes(dataContents));
+            if (shouldSeekToDataStart)
             {
-                foreach (ValidationFeedbackValue instance in validationFeedback.Value)
-                {
-                    Logger.LogMessage($"Line {instance.Line}, Char {instance.Character}: "
-                        + $"{validationFeedback.Key.Rule} {instance.AdditionalInfo}");
-                }
+                stream.Seek(DataStartOffset, SeekOrigin.Begin);
             }
-            Assert.HasCount(0, validationFeedbacks);
-        }
 
-        [TestMethod]
-        public async Task TestValidateAsyncWithoutErrors()
-        {
-            using Stream stream = new MemoryStream(Encoding.UTF8.GetBytes(DataStreamContents.SIMPLE_VALID_DATA));
-            stream.Seek(6, 0);
             DataValidator validator = new(5, 4, 1);
 
             ValidationResult result = await validator.ValidateAsync(stream, "foo", Encoding.UTF8, cancellationToken: TestContext.CancellationToken);
             ValidationFeedback validationFeedbacks = result.FeedbackItems;
+            int actualErrorCount = validationFeedbacks.Values.SelectMany(f => f).Count();
 
-            foreach (KeyValuePair<ValidationFeedbackKey, List<ValidationFeedbackValue>> validationFeedback in validationFeedbacks)
-            {
-                foreach (ValidationFeedbackValue instance in validationFeedback.Value)
-                {
-                    Logger.LogMessage($"Line {instance.Line}, Char {instance.Character}: "
-                        + $"{validationFeedback.Key.Rule} {instance.AdditionalInfo}");
-                }
-            }
-            Assert.HasCount(0, validationFeedbacks);
-        }
-
-        [TestMethod]
-        public async Task TestValidateAsyncWithoutMissingCodeDelimetersReturnsWithoutErrors()
-        {
-            using Stream stream = new MemoryStream(Encoding.UTF8.GetBytes(DataStreamContents.SIMPLE_VALID_DATA_WITHOUT_MISISNG_CODE_DELIMETERS));
-            stream.Seek(6, 0);
-            DataValidator validator = new(5, 4, 1);
-
-            ValidationResult result = await validator.ValidateAsync(stream, "foo", Encoding.UTF8, cancellationToken: TestContext.CancellationToken);
-            ValidationFeedback validationFeedbacks = result.FeedbackItems;
-
-            foreach (KeyValuePair<ValidationFeedbackKey, List<ValidationFeedbackValue>> validationFeedback in validationFeedbacks)
-            {
-                foreach (ValidationFeedbackValue instance in validationFeedback.Value)
-                {
-                    Logger.LogMessage($"Line {instance.Line}, Char {instance.Character}: "
-                        + $"{validationFeedback.Key.Rule} {instance.AdditionalInfo}");
-                }
-            }
-            Assert.HasCount(0, validationFeedbacks);
-        }
-
-        [TestMethod]
-        public void TestValidateWithErrors()
-        {
-            using Stream stream = new MemoryStream(Encoding.UTF8.GetBytes(DataStreamContents.SIMPLE_INVALID_DATA));
-            stream.Seek(6, 0); 
-            DataValidator validator = new(5, 4, 1);
-
-            ValidationFeedback validationFeedbacks = validator.Validate(stream, "foo", Encoding.UTF8).FeedbackItems;
-
-            foreach (KeyValuePair<ValidationFeedbackKey, List<ValidationFeedbackValue>> validationFeedback in validationFeedbacks)
-            {
-                foreach (ValidationFeedbackValue instance in validationFeedback.Value)
-                {
-                    Logger.LogMessage($"Line {instance.Line}, Char {instance.Character}: "
-                        + $"{validationFeedback.Key.Rule} {instance.AdditionalInfo}");
-                }
-            }
-
-            Assert.HasCount(7, validationFeedbacks); // Unique feedbacks
-            Assert.HasCount(12, validationFeedbacks.Values.SelectMany(f => f)); // Total feedbacks including duplicates
-        }
-
-        [TestMethod]
-        public async Task TestValidateAsyncWithErrors()
-        {
-            using Stream stream = new MemoryStream(Encoding.UTF8.GetBytes(DataStreamContents.SIMPLE_INVALID_DATA));
-            stream.Seek(6, 0);
-            DataValidator validator = new(5, 4, 1);
-
-            ValidationResult result = await validator.ValidateAsync(stream, "foo", Encoding.UTF8, cancellationToken: TestContext.CancellationToken);
-            ValidationFeedback validationFeedbacks = result.FeedbackItems;
-
-            foreach (KeyValuePair<ValidationFeedbackKey, List<ValidationFeedbackValue>> validationFeedback in validationFeedbacks)
-            {
-                foreach (ValidationFeedbackValue instance in validationFeedback.Value)
-                {
-                    Logger.LogMessage($"Line {instance.Line}, Char {instance.Character}: "
-                        + $"{validationFeedback.Key.Rule} {instance.AdditionalInfo}");
-                }
-            }
-
-            Assert.HasCount(7, validationFeedbacks); // Unique feedbacks
-            Assert.HasCount(12, validationFeedbacks.Values.SelectMany(f => f)); // Total feedbacks including duplicates
-        }
-
-        [TestMethod]
-        public void ValidateWithoutDataReturnsErrors()
-        {
-            using Stream stream = new MemoryStream(Encoding.UTF8.GetBytes(DataStreamContents.NO_DATA));
-            DataValidator validator = new(5, 4, 1);
-
-            ValidationFeedback validationFeedbacks = validator.Validate(stream, "foo", Encoding.UTF8).FeedbackItems;
-
-            foreach (KeyValuePair<ValidationFeedbackKey, List<ValidationFeedbackValue>> validationFeedback in validationFeedbacks)
-            {
-                foreach (ValidationFeedbackValue instance in validationFeedback.Value)
-                {
-                    Logger.LogMessage($"Line {instance.Line}, Char {instance.Character}: "
-                        + $"{validationFeedback.Key.Rule} {instance.AdditionalInfo}");
-                }
-            }
-
-            Assert.HasCount(2, validationFeedbacks);
-        }
-
-        [TestMethod]
-        public async Task ValidateAsyncWithoutDataReturnsErrors()
-        {
-            using Stream stream = new MemoryStream(Encoding.UTF8.GetBytes(DataStreamContents.NO_DATA));
-            DataValidator validator = new(5, 4, 1);
-
-            ValidationResult result = await validator.ValidateAsync(stream, "foo", Encoding.UTF8, cancellationToken: TestContext.CancellationToken);
-            ValidationFeedback validationFeedbacks = result.FeedbackItems;
-
-            foreach (KeyValuePair<ValidationFeedbackKey, List<ValidationFeedbackValue>> validationFeedback in validationFeedbacks)
-            {
-                foreach (ValidationFeedbackValue instance in validationFeedback.Value)
-                {
-                    Logger.LogMessage($"Line {instance.Line}, Char {instance.Character}: "
-                        + $"{validationFeedback.Key.Rule} {instance.AdditionalInfo}");
-                }
-            }
-
-            Assert.HasCount(2, validationFeedbacks);
-        }
-
-        [TestMethod]
-        public void ValidateWithDataOnOnSingleRowReturnsErrors()
-        {
-            using Stream stream = new MemoryStream(Encoding.UTF8.GetBytes(DataStreamContents.DATA_ON_SINGLE_ROW));
-            DataValidator validator = new(5, 4, 1);
-
-            ValidationFeedback validationFeedbacks = validator.Validate(stream, "foo", Encoding.UTF8).FeedbackItems;
-
-            foreach (KeyValuePair<ValidationFeedbackKey, List<ValidationFeedbackValue>> validationFeedback in validationFeedbacks)
-            {
-                foreach (ValidationFeedbackValue instance in validationFeedback.Value)
-                {
-                    Logger.LogMessage($"Line {instance.Line}, Char {instance.Character}: "
-                        + $"{validationFeedback.Key.Rule} {instance.AdditionalInfo}");
-                }
-            }
-
-            Assert.HasCount(2, validationFeedbacks); // Unique feedbacks
-            Assert.HasCount(6, validationFeedbacks.Values.SelectMany(f => f)); // Total feedbacks including duplicates
-        }
-
-        [TestMethod]
-        public async Task ValidateAsyncWithDataOnSingleRowReturnsErrors()
-        {
-            using Stream stream = new MemoryStream(Encoding.UTF8.GetBytes(DataStreamContents.DATA_ON_SINGLE_ROW));
-            DataValidator validator = new(5, 4, 1);
-
-            ValidationResult result = await validator.ValidateAsync(stream, "foo", Encoding.UTF8, cancellationToken: TestContext.CancellationToken);
-            ValidationFeedback validationFeedbacks = result.FeedbackItems;
-
-            foreach (KeyValuePair<ValidationFeedbackKey, List<ValidationFeedbackValue>> validationFeedback in validationFeedbacks)
-            {
-                foreach (ValidationFeedbackValue instance in validationFeedback.Value)
-                {
-                    Logger.LogMessage($"Line {instance.Line}, Char {instance.Character}: "
-                        + $"{validationFeedback.Key.Rule} {instance.AdditionalInfo}");
-                }
-            }
-
-            Assert.HasCount(2, validationFeedbacks);// Unique feedbacks
-            Assert.HasCount(6, validationFeedbacks.Values.SelectMany(f => f)); // Total feedbacks including duplicates
-        }
-
-        [TestMethod]
-        public void ValidateDataStartingWithEnclosedMissingValueReturnsWithoutErrors()
-        {
-            using Stream stream = new MemoryStream(Encoding.UTF8.GetBytes(DataStreamContents.DATA_STARTING_WITH_ENCLOSED_MISSING_VALUE));
-            stream.Seek(6, 0);
-            DataValidator validator = new(5, 4, 1);
-
-            ValidationFeedback validationFeedbacks = validator.Validate(stream, "foo", Encoding.UTF8).FeedbackItems;
-
-            Assert.HasCount(0, validationFeedbacks);
-        }
-
-        [TestMethod]
-        public void ValidateDataStartingWithUnenclosedMissingValueReturnsWithoutErrors()
-        {
-            using Stream stream = new MemoryStream(Encoding.UTF8.GetBytes(DataStreamContents.DATA_STARTING_WITH_UNENCLOSED_MISSING_VALUE));
-            stream.Seek(6, 0);
-            DataValidator validator = new(5, 4, 1);
-
-            ValidationFeedback validationFeedbacks = validator.Validate(stream, "foo", Encoding.UTF8).FeedbackItems;
-
-            Assert.HasCount(0, validationFeedbacks);
-        }
-
-        [TestMethod]
-        public void ValidateDataStartingWithNilValueReturnsWithoutErrors()
-        {
-            using Stream stream = new MemoryStream(Encoding.UTF8.GetBytes(DataStreamContents.DATA_STARTING_WITH_NIL_VALUE));
-            stream.Seek(6, 0);
-            DataValidator validator = new(5, 4, 1);
-
-            ValidationFeedback validationFeedbacks = validator.Validate(stream, "foo", Encoding.UTF8).FeedbackItems;
-
-            Assert.HasCount(0, validationFeedbacks);
-        }
-
-        [TestMethod]
-        public void ValidateDataStartingWithUnenclosedNilValueReturnsWithoutErrors()
-        {
-            using Stream stream = new MemoryStream(Encoding.UTF8.GetBytes(DataStreamContents.DATA_STARTING_WITH_UNENCLOSED_NIL_VALUE));
-            stream.Seek(6, 0);
-            DataValidator validator = new(5, 4, 1);
-
-            ValidationFeedback validationFeedbacks = validator.Validate(stream, "foo", Encoding.UTF8).FeedbackItems;
-
-            Assert.HasCount(0, validationFeedbacks);
-        }
-
-        [TestMethod]
-        public async Task ValidateAsyncDataStartingWithEnclosedMissingValueReturnsWithoutErrors()
-        {
-            using Stream stream = new MemoryStream(Encoding.UTF8.GetBytes(DataStreamContents.DATA_STARTING_WITH_ENCLOSED_MISSING_VALUE));
-            stream.Seek(6, 0);
-            DataValidator validator = new(5, 4, 1);
-
-            ValidationResult result = await validator.ValidateAsync(stream, "foo", Encoding.UTF8, cancellationToken: TestContext.CancellationToken);
-
-            Assert.HasCount(0, result.FeedbackItems);
-        }
-
-        [TestMethod]
-        public async Task ValidateAsyncDataStartingWithUnenclosedMissingValueReturnsWithoutErrors()
-        {
-            using Stream stream = new MemoryStream(Encoding.UTF8.GetBytes(DataStreamContents.DATA_STARTING_WITH_UNENCLOSED_MISSING_VALUE));
-            stream.Seek(6, 0);
-            DataValidator validator = new(5, 4, 1);
-
-            ValidationResult result = await validator.ValidateAsync(stream, "foo", Encoding.UTF8, cancellationToken: TestContext.CancellationToken);
-
-            Assert.HasCount(0, result.FeedbackItems);
+            Assert.HasCount(expectedUniqueErrorCount, validationFeedbacks);
+            Assert.AreEqual(expectedTotalErrorCount, actualErrorCount);
         }
     }
 }
